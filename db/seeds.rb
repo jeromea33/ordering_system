d4c33058a4a7802a37fb54a1534bd1693c778dd2
Jerome Angeles <raizumaraiz@gmail.com>
2015-09-01 19:25:34 +0800

    Second Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+x = AdminUser.new
+x.username = "admin"
+x.password = "admin"
+x.save
